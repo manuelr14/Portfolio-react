1296a6ae4d7dad4690988b758e6c7e6cde5a27e4
Manuel <Ramirez>
2020-04-30 16:34:27 -0400

    Updates

---
+++ precache-manifest.31834f9c20485fcef062b32a744afd05.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631a78c95b3e197e77e7afa9d27e5968",
+    "url": "/Portfolio-react/index.html"
+  },
+  {
+    "revision": "d0d173b2575996c43bd0",
+    "url": "/Portfolio-react/static/css/main.13f55b07.chunk.css"
+  },
+  {
+    "revision": "4f0c5af35845ac4ad1bd",
+    "url": "/Portfolio-react/static/js/2.84d083df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-react/static/js/2.84d083df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d173b2575996c43bd0",
+    "url": "/Portfolio-react/static/js/main.9ba1a840.chunk.js"
+  },
+  {
+    "revision": "dec0d582b0cdd8c19d9a",
+    "url": "/Portfolio-react/static/js/runtime-main.dda6cdde.js"
+  },
+  {
+    "revision": "22a3b8c652f48058651bc38e89fb9778",
+    "url": "/Portfolio-react/static/media/MRamirez_Resume.22a3b8c6.pdf"
+  },
+  {
+    "revision": "5a5a81889427089c1abed855c4c66245",
+    "url": "/Portfolio-react/static/media/burguer-app.5a5a8188.png"
+  },
+  {
+    "revision": "6d4e435b65b22603afddbcb684806fb1",
+    "url": "/Portfolio-react/static/media/choice-maker.6d4e435b.png"
+  },
+  {
+    "revision": "59821560fafdf17b5dc03074f16552e1",
+    "url": "/Portfolio-react/static/media/contact-background.59821560.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b10dc432ab9a8dd7a5e8c539797485d3",
+    "url": "/Portfolio-react/static/media/foto1.b10dc432.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/Portfolio-react/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/Portfolio-react/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/Portfolio-react/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/Portfolio-react/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "6bc65280af01576c1ed289407ec01c0e",
+    "url": "/Portfolio-react/static/media/mancomputer.6bc65280.jpg"
+  },
+  {
+    "revision": "40f8c4c84adc32b8290d2f98dc112ba8",
+    "url": "/Portfolio-react/static/media/password-Generator.40f8c4c8.png"
+  },
+  {
+    "revision": "df103e8c53724c51508194b534ce9cf9",
+    "url": "/Portfolio-react/static/media/profile-picture.df103e8c.png"
+  },
+  {
+    "revision": "24ec2a74aa557780291839485744e2db",
+    "url": "/Portfolio-react/static/media/weather-dashboard.24ec2a74.png"
+  },
+  {
+    "revision": "84a72dbf58024fbfe854ec67565bc619",
+    "url": "/Portfolio-react/static/media/work-daySchedule.84a72dbf.png"
+  },
+  {
+    "revision": "438f196e54945752a0e93702114887b9",
+    "url": "/Portfolio-react/static/media/yummtrucks-img.438f196e.png"
+  }
+]);
